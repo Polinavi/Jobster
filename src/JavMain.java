ebbd27f2a91d0da25f1546f14ea828cf04eeb2b0
polinavirchenko <polinavirchenko@gmail.com>
2021-03-06 12:23:10 -0500

    bly

--- src/JavMain.java
+++ src/JavMain.java
@@ -1,6 +1,6 @@
 public class JavMain {
     public static void main(String[] args) {
         System.out.println("Hello Mazafuc");
-        System.out.println("Bly");
+        System.out.println("Bl");
     }
 }
