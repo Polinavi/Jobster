6e499220979be633aa11581950fb61b7c433047d
polinavirchenko <polinavirchenko@gmail.com>
2021-03-07 10:34:53 -0500

    ignore

--- src/JavMain.java
+++ src/JavMain.java
@@ -1,7 +1,9 @@
 public class JavMain {
     public static void main(String[] args) {
-        System.out.println("Hello Mazafuc");
-        System.out.println("");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+
+        }
 
 
 
